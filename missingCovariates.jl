a8a49969c4ad85d7e86e64fe98f03a60521006ef
jmrmcode <juanmimullor@gmail.com>
2021-09-17 15:56:46 -0400

    dic

--- missingCovariates.jl
+++ missingCovariates.jl
@@ -17,10 +17,28 @@ end
 # x[1] is a parameter, but x[2] is an observation
 model = gdemo(Array(df.pptAPR3LAG))
 c = sample(model, NUTS(), 3000)
+
+# Deviance Information Criterion (DIC)
+lpfun = function f(chain::Chains) # function to compute the logpdf values
+    niter, nparams, nchains = size(chain)
+    lp = zeros(niter + nchains) # resulting logpdf values
+    for i = 1:nparams
+        lp += map(p -> logpdf.(Normal(sumstats[2, :mean], sumstats[1, :mean]), df.pptAPR3LAG), Array(chain[:,i,:]))
+    end
+    return lp
+end
+DIC, pD = dic(c, lpfun)
+
+# JLSO saves also metadata to import later to different Julia versions. However, the line below does not work
+# JLSO.save("U:/juanmi/cobrilha_project/models/test.jlso", c)
+# The two lines below only make sense to export Julia objects between the same Julia versions
+Serialization.serialize("U:/juanmi/cobrilha_project/models/c.jls",c) # export
+Serialization.deserialize("U:/juanmi/cobrilha_project/models/c.jls") # import
+
 # get posterior means of the predicted values]
 using Statistics
 sumstats = summarize(c, mean)
-means = sumstats[3:13, :mean]
+means = sumstats[3:16, :mean]
 # replace missing with the predicted values
 df.pptAPR3LAG[findall(ismissing, df.pptAPR3LAG)] .= means
 findall(ismissing, df.pptAPR3LAG)
