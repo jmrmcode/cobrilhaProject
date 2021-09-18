7b5e409af71d7a6bf65ffb79d44c54d31013143e
jmrmcode <juanmimullor@gmail.com>
2021-09-17 22:49:05 -0400

    DIC

--- missingCovariates.jl
+++ missingCovariates.jl
@@ -18,23 +18,6 @@ end
 model = gdemo(Array(df.pptAPR3LAG))
 c = sample(model, NUTS(), 3000)
 
-# Deviance Information Criterion (DIC)
-lpfun = function f(chain::Chains) # function to compute the logpdf values
-    niter, nparams, nchains = (3000, 16, 1)
-    lp = zeros(niter + nchains) # resulting logpdf values
-    for i = 1:nparams
-        lp += map(p -> logpdf.(Normal(sumstats[2, :mean], sumstats[1, :mean]), df.pptAPR3LAG), Array(chain[:,i,:])) # Compute the sum of log-densities at each iteration of MCMC output for stochastic nodes.
-    end
-    return lp
-end
-DIC, pD = dic(c, lpfun)
-
-# JLSO saves also metadata to import later to different Julia versions. However, the line below does not work
-# JLSO.save("U:/juanmi/cobrilha_project/models/test.jlso", c)
-# The two lines below only make sense to export Julia objects between the same Julia versions
-Serialization.serialize("U:/juanmi/cobrilha_project/models/c.jls",c) # export
-Serialization.deserialize("U:/juanmi/cobrilha_project/models/c.jls") # import
-
 # get posterior means of the predicted values]
 using Statistics
 sumstats = summarize(c, mean)
@@ -42,6 +25,30 @@ means = sumstats[3:16, :mean]
 # replace missing with the predicted values
 df.pptAPR3LAG[findall(ismissing, df.pptAPR3LAG)] .= means
 findall(ismissing, df.pptAPR3LAG)
+
+# Deviance Information Criterion (DIC) and number of effective parameter
+    niter = size(c)[1]
+    lp = zeros(niter) # resulting logpdf values
+    for i = 1:3000
+        lp[i] = sum(map((m,s) -> logpdf.(Normal(m, s), df.pptAPR3LAG), Array(c[:,2,:]), Array(c[:,1,:]))[i]) # Compute the sum of log-densities at each iteration of MCMC output for stochastic nodes.
+    end
+
+    # D = Deviance = -2 * loglikehood
+    D = -2 * lp
+    D̄ = mean(D)
+    # effective number of parameters pᵩ = D̄ + D_θ_hat = mean deviamce + deviance of the parameters mean
+    D_θ_hat = -2 * sum(map((m,s) -> logpdf.(Normal(m, s), df.pptAPR3LAG), mean(Array(c[:,2,:])), mean(Array(c[:,1,:]))))
+    pᵩ = D̄ + D_θ_hat
+    # deviance information criterion (DIC)
+    DIC = D̄ + pᵩ
+
+# JLSO saves also metadata to import later to different Julia versions. However, the line below does not work
+# JLSO.save("U:/juanmi/cobrilha_project/models/test.jlso", c)
+# The two lines below only make sense to export Julia objects between the same Julia versions
+Serialization.serialize("U:/juanmi/cobrilha_project/models/c.jls",c) # export
+Serialization.deserialize("U:/juanmi/cobrilha_project/models/c.jls") # import
+
+
 ##
 # pptAPR3LAG
 @model function gdemo(x, ::Type{T} = Float64) where {T}
